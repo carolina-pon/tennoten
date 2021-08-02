1d48385f9ff4cd253c31d8e5b1d3a0187f72c3c1
nagata <mad.20th@gmail.com>
2021-08-02 11:01:29 +0900

    add 作品CRUD機能

---
+++ app/controllers/works_controller.rb
@@ -0,0 +1,51 @@
+class WorksController < ApplicationController
+  def index
+    @works = Work.all
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+    if @work.save
+      redirect_to works_path, success: '投稿しました'
+    else
+      flash.now[:alert] = '投稿に失敗しました'
+      render :new
+    end
+    end
+
+  def edit
+    @work = Work.find(params[:id])
+  end
+
+  def update
+    @work = Work.find(params[:id])
+    if @work.update(work_params)
+      redirect_to works_path, notice: '投稿を更新しました'
+    else
+      flash.now[:alert] = '投稿の更新に失敗しました'
+      render :edit
+    end
+  end
+
+  def show
+    @work = Work.find(params[:id])
+  end
+
+
+  def destroy
+    @work = Work.find(params[:id])
+    @work.destroy!
+    redirect_to works_path, notice: '投稿を削除しました'
+  end
+
+  private
+
+  def work_params
+    # images:[] とすることで、JSON形式でparamsを受け取れる
+    params.require(:work).permit(:title, :description, :material, :size, :weight, images: [])
+  end
+end
