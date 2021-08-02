e6a309ae796492653ac658e650bfd732b7543d26
nagata <mad.20th@gmail.com>
2021-08-02 14:02:55 +0900

    add 挨拶ページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  root 'works#index'
+  root 'greetings#index'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users, only: %i[index]
   resources :authors, only: %i[index new create edit update destroy]
   resources :works
+  resources :greetings, only: %i[index]
 end
